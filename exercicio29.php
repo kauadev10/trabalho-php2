eb8ee0ec0af266f1b2b99e1f0a11e68f51897711
Kauã Fagundes <kauan2542@gmail.com>
2025-10-19 20:12:11 -0300

    add parte do bruno

---
+++ exercicio29.php
@@ -0,0 +1,29 @@
+<?php
+
+/******************************************************************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2B
+    Componentes:
+        25061077-2 - Kauã Ricardo Gomes Fagundes
+        25010975-2 - Bruno Petroli
+        25004001-3 - Rafael Diesel
+        25229846-2 - Wendel Souza Cardoso
+        25228676-2 - Marlon Willian Silva Barros
+        25357682-2 - Eduardo Rupp da Luz
+        25165088-2 - Vinicius Bastos Rodrigues
+        25178065-2 - Marcos Barcelar
+        22001126-3 - Eric Ruthes
+    Data: 29 de Agosto de 2025
+    Descritivo: Dado um array de números [12, 5, 23, 18, 9, 45], imprima apenas os números maiores que 20.
+*******************************************************************************/
+
+$numeros = [12, 5, 23, 18, 9, 45];
+foreach ($numeros as $numero) {
+    if ($numero > 20) {
+        echo $numero . "\n";
+    }
+}
+
+?>          
---
+++ exercicio30.php
@@ -0,0 +1,29 @@
+<?php
+
+/******************************************************************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2B
+    Componentes:
+        25061077-2 - Kauã Ricardo Gomes Fagundes
+        25010975-2 - Bruno Petroli
+        25004001-3 - Rafael Diesel
+        25229846-2 - Wendel Souza Cardoso
+        25228676-2 - Marlon Willian Silva Barros
+        25357682-2 - Eduardo Rupp da Luz
+        25165088-2 - Vinicius Bastos Rodrigues
+        25178065-2 - Marcos Barcelar
+        22001126-3 - Eric Ruthes
+    Data: 29 de Agosto de 2025
+    Descritivo: Array Multidimensional:Crie uma matriz 3x3 (array de arrays) e preencha-a com números. Imprima o elemento da segunda linha e terceira coluna
+*******************************************************************************/
+
+$matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+echo $matriz[1][2];
+
+?>          
